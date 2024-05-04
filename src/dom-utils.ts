0679ebc6ebd066e35331c8924d6d6c94bb9455c8
celinamaxh-dotcom <celinamaxhuni@gmx.de>
2024-05-04 14:08:16 +0200

    when the createTeam button is clicked, the Input field is shown

--- src/dom-utils.ts
+++ src/dom-utils.ts
@@ -1,1 +1,5 @@
 export const textInput = document.querySelector('#myInput') as HTMLInputElement;
+export const TeamInput = document.querySelector(".Team") as HTMLDivElement;
+export const create = document.querySelector("#create") as HTMLButtonElement;
+export const teamNameInput = document.querySelector("#teamName") as HTMLInputElement;
+
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 // Imports use relative file paths or Node.js package names
 import { textInput } from './dom-utils';
+import { TeamInput } from './dom-utils';
+import { create } from './dom-utils';
+import { teamNameInput } from './dom-utils';
 // CSS IMPORT IN TS NUR ÜBER VITE MÖGLICH
 import './styles/styles.css';
 
@@ -7,7 +10,17 @@ import './styles/styles.css';
 //THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE
 
 // init App
-textInput.addEventListener('input', (e) => {
-    //log input value
-    console.log((e.target as HTMLInputElement).value);
-})
+// textInput.addEventListener('input', (e) => {
+//     //log input value
+//     console.log((e.target as HTMLInputElement).value);
+// })
+
+//my Code
+
+create.addEventListener("click", () => {
+    if (TeamInput.style.display = "none")
+        TeamInput.style.display = "block";
+        console.log("it worked");
+    // teamNameInput.style.display = "block";
+    // teamNameInput.value = "";
+});
